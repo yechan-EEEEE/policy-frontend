a463de07c4ba6315f7e1e8c830398c1c2bc0ca43
junDevCodes <jundevcodes@gmail.com>
2025-12-23 09:27:42 +0900

    edit_policies_urls /list>/

--- backend/policies/urls.py
+++ backend/policies/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'policies'
 
 urlpatterns = [
-    path('list/', views.policy_list, name='policy_list'),
+    path('', views.policy_list, name='policy_list'),
     path('<str:plcyNo>/', views.policy_detail, name='policy_detail'),
     path('<str:plcyNo>/like/', views.policy_like, name='policy_like'),
     path('recommend/ai/', views.policy_recommend, name='policy_recommend'),
